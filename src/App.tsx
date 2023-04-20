516a414c2f81b28341fb97bd0d8a7a646b70b537
Termi7 <90416021+Termi7@users.noreply.github.com>
2023-04-20 06:20:09 -0400

    added sources files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://localhost:8000", // replace with your FastAPI endpoint URL
+});
 
-function App() {
+function App(): JSX.Element {
+  type ApiResponse = {
+    Name: string;
+    "Scientific Name": string;
+    Population: number;
+    "Extinction Status": string;
+    Type: string;
+    Diet: string;
+    Location: string;
+    Facts: string[];
+  };
+
+  const [file, setFile] = useState<File | null>(null);
+  const [responseText, setResponseText] = useState<ApiResponse | null>(null);
+
+  const [imageUrl, setImageUrl] = useState<string>("");
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files ? event.target.files[0] : null;
+    setFile(selectedFile);
+    if (selectedFile) {
+      const url = URL.createObjectURL(selectedFile);
+      setImageUrl(url);
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!file) {
+      return;
+    }
+
+    console.log(22);
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await api.post("/uploadfile/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log(response.data);
+      console.log(22);
+      setResponseText(response.data); // set the response data in state
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+      </form>
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt="Selected file"
+          style={{ width: 200, height: 200 }}
+        />
+      )}
+      {responseText && (
+        <div>
+          <h2>{responseText.Name}</h2>
+          <p>Scientific Name: {responseText["Scientific Name"]}</p>
+          <p>Population: {responseText.Population}</p>
+          <p>Extinction Status: {responseText["Extinction Status"]}</p>
+          <p>Type: {responseText.Type}</p>
+          <p>Diet: {responseText.Diet}</p>
+          <p>Location: {responseText.Location}</p>
+          <h3>Facts:</h3>
+          {responseText.Facts && (
+            <ul>
+              {responseText.Facts.map((fact, index) => (
+                <li key={index}>{fact}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
     </div>
   );
 }
